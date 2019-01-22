dd6324efc55130c20f1b1d027fe18fc2ca697ca8
Mario Lema <lema_mario@yahoo.com>
2019-01-22 21:39:52 +0000

    add websites edit

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ import jqueryLogo from "./images/jquery-logo.png";
 import reactLogo from "./images/react-logo.png";
 import screenLogo from "./images/screen-logo.png";
 import githubLogo from "./images/github-logo.png";
-import webImage from "./images/website frame-grid.svg";
 import codepenLogo from "./images/codepen-logo.svg";
 import linkedinLogo from "./images/linkedin-logo.svg";
+import coreCorrectImage from "./images/core-correct.jpg";
+import calculatorImage from "./images/calculator.jpg"
+import linuxMintImage from "./images/linux-mint.jpg"
+import PomodoroImage from "./images/pomodoro-timer.jpg"
+import typhonImage from "./images/typhon.jpg"
+import weatherCardsImage from "./images/weather-cards.jpg"
+import 'simplebar';
+import 'simplebar/dist/simplebar.min.css';
 import "./App.css";
 
 class App extends Component {
@@ -22,52 +29,52 @@ class App extends Component {
           title: "hello"
         }, 
         pomodoroClock:{
-          title: "Pomodoro Clock",
-          media: webImage,
+          title: "POMODORO TIMER",
+          media: PomodoroImage,
           libraries: [htmlLogo,cssLogo,jsLogo,reactLogo],
           description: "text",
           pageLink: "",
-          githubLink: "",
+          githubLink: "https://github.com/MarioLema/pomodoro-app",
         }, 
         weatherCards:{
-          title: "Weather Cards",
-          media: webImage,
+          title: "WEATHER CARDS",
+          media: weatherCardsImage,
           libraries: [htmlLogo,cssLogo,jsLogo,reactLogo],
           description: "text",
           pageLink: "",
-          githubLink: "",
+          githubLink: "https://github.com/MarioLema/mars-react",
         }, 
         calculator:{
-          title: "Calculator",
-          media: webImage,
+          title: "CALCULATOR",
+          media: calculatorImage,
           libraries: [htmlLogo,cssLogo,jsLogo,reactLogo],
           description: "text",
           pageLink: "",
-          githubLink: "",
+          githubLink: "https://github.com/MarioLema/react-calculator",
         }, 
         linuxMint:{
-          title: "Linux Mint",
-          media: webImage,
+          title: "LINUX MINT",
+          media: linuxMintImage,
           libraries: [htmlLogo,cssLogo,jsLogo],
           description: "text",
           pageLink: "",
-          githubLink: "",
+          githubLink: "https://github.com/MarioLema/Linux-mint",
         }, 
         typhon:{
-          title: "Typhon",
-          media: webImage,
+          title: "TYPHON",
+          media: typhonImage,
           libraries: [htmlLogo, cssLogo, jsLogo, bootstrapLogo, jqueryLogo],
           description: "text",
           pageLink: "",
-          githubLink: "",
+          githubLink: "https://github.com/MarioLema/Typhon",
         }, 
         coreCorrectHealth:{
-          title: "Core Correct Health",
-          media: webImage,
+          title: "CORE CORRECT HEALTH",
+          media: coreCorrectImage,
           libraries: [htmlLogo, cssLogo, jsLogo, bootstrapLogo, jqueryLogo],
           description: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer luctus justo in urna condimentum condimentum. Nunc non metus erat. Vivamus a massa nibh. Aliquam tempus tempus posuere. Maecenas id vulputate augue. Proin vitae nisi ligula. Maecenas sit amet bibendum nibh. Curabitur non fermentum turpis. Praesent et varius nibh, sed dignissim ex.Nulla luctus id est sit amet commodo. Maecenas dictum rhoncus justo, quis ultrices nisi fermentum id. Fusce velit diam, tincidunt in vestibulum vehicula, pretium malesuada purus. Maecenas vehicula, nisi nec feugiat posuere, dolor sem elementum orci, ac finibus libero nulla vitae justo.",
           pageLink: "",
-          githubLink: "",
+          githubLink: "https://github.com/MarioLema/core-correct",
         }, 
         contact:{
           title: "hello"
@@ -108,7 +115,7 @@ class NavigationMenu extends App {
         <ul className="menu-list">
           <li className="list-link">Who am I</li>
           <li>
-            Published Websites
+            Live Websites
             <ul>
               <li className="list-link">Core Correct Health</li>
             </ul>
@@ -140,7 +147,7 @@ class WebsitePage extends App {
     return (
       <div className="page-container">
       <h1 className="section-title">{this.props.activePage.title}</h1>
-      <div className="image-container">
+      <div className="image-container" data-simplebar>
         <img className="web-image" src={this.props.activePage.media} alt="" />
       </div>
       <div className="description-container">
@@ -148,15 +155,14 @@ class WebsitePage extends App {
         <div className="logos-container">
         {this.props.activePage.libraries.map( (logo,index) => <img src={logo} alt="" className="logo-image" key={`logo ${index}`}/> )}
         </div>
-        <h3>DESCRIPTION</h3>
-        <p>{this.props.activePage.description}
+        <p className="web-description">{this.props.activePage.description}
         </p>
         <h3>LINKS</h3>
         <div className="logos-container">
-          <a href={this.props.activePage.pageLink}>
+          <a href={this.props.activePage.pageLink} target="_blank" rel="noopener noreferrer">
             <img src={screenLogo} alt="" className="logo-image code-link" />
           </a>
-          <a href={this.props.activePage.githubLink}>
+          <a href={this.props.activePage.githubLink} target="_blank" rel="noopener noreferrer">
             <img src={githubLogo} alt="" className="logo-image code-link" />
           </a>
         </div>
